c6fdab163d5cf2646ed05f2d1b7e5b8939aba450
EliGray20 <115156773+EliGray20@users.noreply.github.com>
2022-10-06 09:02:28 -0400

    Create Story2.py

---
+++ Story2.py
@@ -0,0 +1,16 @@
+from Getters import *
+
+def Story1(debug = False):
+    if debug : print("Story1 Function")
+    
+    print("")
+    friendName1 = getWord("enter a name ", debug)
+    sport1 = getSport("enter a sport ", debug)
+    food1 = getFood("enter a fast food resteraunt ", debug)
+    
+    out = "\n"
+    out+= "One day my friend, " +friendName1
+    out+= " were out playing " + sport1 + ".\n"
+    out+= "When we finished playing, I wanted to eat at " + food1 + ".\n"
+    
+    return out
