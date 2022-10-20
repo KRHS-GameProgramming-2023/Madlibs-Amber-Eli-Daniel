f2eb5bd9a7f3578694bb4cedc5e75e44dd5bf66f
Amber Houle <amberrhoule@gmail.com>
2022-10-20 08:00:12 -0400

    Added the storyline to story 4

--- Story4.py
+++ Story4.py
@@ -5,6 +5,26 @@ def Story4(debug = False):
     
     print("\n")
     
-    out = "Test"
+    out = "Once upon a time there was a " + noun1
+    out += " and this " + noun1
+    out += " loved to eat " + food1
+    out += " as well as " + food2
+    out += " because of this, the " + noun1
+    out += " was named " + name1
+    out += ". One day, " + name1
+    out += " was eating at " + restaurant1
+    out += " unfortunately, while eating, a " + creature1
+    out += " decided to attack. Luckily, it wasn't a real attack, the " + creature1
+    out += " just wanted to play " + game1
+    out += " sadly, everyone was too scared to play with them and everyone ended up " + movementVerb1
+    out += " away. Later on, " + name2
+    out += ", the best friend of the " + creature1
+    out += " wanted to cheer up their friend, so they got everyone together and they all played " + game2
+    out += " while eating " + food1
+    out += " and " + food2
+    out += " they all got so full that everyone became extremely " + adjective1
+    out += " to counteract this, they decided to go play " + sport1
+    out += " they ended up playing till the end of eternity."
+    out += "\n \n The End"
     
     return out
